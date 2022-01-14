5edcb5bfd059269044366d7cb7d1288e7016b674
Lucas Aprigio <lucsaprigio@hotmail.com>
2022-01-14 17:19:38 -0300

    database

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,10 @@
+import "reflect-metadata";
 import express from 'express';
+
+import './database';
 
 const app = express();
 
-/* 
-* GET => Buscar uma informação dentro da API
-* POST => Adicionar/inserir uma informação dentro da API
-* PUT => Alterar uma informação dentro da API
-* DELETE => Deletar uma informação.
-* PATCH => Alterar uma informação específica (Somente a senha exemplo)
-*/
-
-app.get('/test', (request, response) => {
-    // Request => Entrando
-    // Response => Saindo
-    return response.send('Olá NLW');
-
-});
-
-app.post('/test-post', (request, response) => {
-    return response.send('Olá NLW método post')
-});
-
 app.listen(3000, () => 
     console.log("Server is running  🚀"
-))
+));
